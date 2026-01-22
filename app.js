58178fa2fa34a64881a6f2042fddde3b649b89c2
yuvika chaudhary <yuvika1904@gmail.com>
2026-01-23 00:01:17 +0530

    question3

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-let {writewordcount} = require ("./q1.js");
-writewordcount();
-const stringUtils = require("./q2.js");
-console.log(stringUtils.capital("yuvika"));
-console.log(stringUtils.reverses("chaudhary"));
-console.log(stringUtils.vowel("yuvika"));
+// let {writewordcount} = require ("./q1.js");
+// writewordcount();
+// const stringUtils = require("./q2.js");
+// console.log(stringUtils.capital("yuvika"));
+// console.log(stringUtils.reverses("chaudhary"));
+// console.log(stringUtils.vowel("yuvika"));
+let {time} = require ("./q3.js");
+time();
