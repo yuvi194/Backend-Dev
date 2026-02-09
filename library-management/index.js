44cb80424bb1d86c8b796c8c649e5c5373662cc5
yuvika chaudhary <yuvika1904@gmail.com>
2026-02-09 11:23:08 +0530

    lbrary-management

---
+++ library-management/index.js
@@ -0,0 +1,6 @@
+import book from "./src/book.js";
+console.log(book("Hunting adeline","H.D.Carlton","Rs.600"));
+import member from "./src/member.js";
+console.log(member("Yuvika","normal"));
+import borrowrecord from "./src/borrowrecord.js";
+console.log(borrowrecord("2026-01-29T09:09:11.572Z","2026-01-29T09:00:08.597Z",2))
